3a318cc7b95a85e78de74f7ff7cabc20b5eaf50f
laszlodus <79lgg4@gmail.com>
2025-09-23 11:01:53 +0100

    update projects

--- main.js
+++ main.js
@@ -1,142 +1,141 @@
-
 // main page background picture load
 
 window.onload = function () {
-    const message = document.querySelector('.head-message');
-    new HeadMessage(message);
+  const message = document.querySelector(".head-message");
+  new HeadMessage(message);
 };
-
 
 // Header message
 
 class HeadMessage {
-    constructor(messageElement) {
-        this.message = messageElement;
-        this.text = "Hello, I'm Laszlo Dus";
-        this.index = 0;
-        this.init();
-    }
+  constructor(messageElement) {
+    this.message = messageElement;
+    this.text = "Hello, I'm Laszlo Dus";
+    this.index = 0;
+    this.init();
+  }
 
-    init() {
-        const interval = setInterval(() => {
-            if (this.index < this.text.length) {
-                this.message.textContent += this.text[this.index];
-                this.index++;
-            } else {
-                clearInterval(interval);
-                
-                setTimeout(() => {
-                    document.querySelector('.new-background').style.transform = "translateY(0)";
-                    this.setColour();
-                }, 200);
-                
-            }
-        }, 50);
-    }
+  init() {
+    const interval = setInterval(() => {
+      if (this.index < this.text.length) {
+        this.message.textContent += this.text[this.index];
+        this.index++;
+      } else {
+        clearInterval(interval);
 
-    setColour() {
-        this.message.classList.add('new-color');
-    }
+        setTimeout(() => {
+          document.querySelector(".new-background").style.transform =
+            "translateY(0)";
+          this.setColour();
+        }, 200);
+      }
+    }, 50);
+  }
+
+  setColour() {
+    this.message.classList.add("new-color");
+  }
 }
 
 // Nav menu
 
 class Menu {
-    constructor(navBtn, navMenu) {
-        this.navBtn = navBtn;
-        this.navMenu = navMenu;
-        this.navLinks = navLinks;
-        this.activeClass = 'active';
-        this.init();
-    }
+  constructor(navBtn, navMenu) {
+    this.navBtn = navBtn;
+    this.navMenu = navMenu;
+    this.navLinks = navLinks;
+    this.activeClass = "active";
+    this.init();
+  }
 
-    init() {
-        this.navBtn.addEventListener('click',() => this.toggleMenu());
-        this.navLinks.forEach(links => {
-            links.addEventListener('click',() => this.closeMenu());
-        });
-    }
+  init() {
+    this.navBtn.addEventListener("click", () => this.toggleMenu());
+    this.navLinks.forEach((links) => {
+      links.addEventListener("click", () => this.closeMenu());
+    });
+  }
 
-    toggleMenu() {
-        this.navMenu.classList.toggle(this.activeClass);
-        this.updateNavBtn();
-    }
+  toggleMenu() {
+    this.navMenu.classList.toggle(this.activeClass);
+    this.updateNavBtn();
+  }
 
-    closeMenu() {
-        this.navMenu.classList.remove(this.activeClass);
-        this.updateNavBtn();
-    }
+  closeMenu() {
+    this.navMenu.classList.remove(this.activeClass);
+    this.updateNavBtn();
+  }
 
-    updateNavBtn() {
-        if (this.navMenu.classList.contains(this.activeClass)) {
-            this.navBtn.innerHTML = '&#10006';
-        } else {
-            this.navBtn.innerHTML = '&#9776;';
-        }
+  updateNavBtn() {
+    if (this.navMenu.classList.contains(this.activeClass)) {
+      this.navBtn.innerHTML = "&#10006";
+    } else {
+      this.navBtn.innerHTML = "&#9776;";
     }
-};
+  }
+}
 
 const navBtn = document.querySelector(".nav-btn");
 const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-menu a");
 const menu = new Menu(navBtn, navMenu);
 
-
 class Form {
-    constructor(textarea, remainCharacter, btn) {
-        this.text = textarea;
-        this.character = textarea.maxLength;
-        this.remain = remainCharacter;
-        this.btn = btn;
-        
-        this.text.addEventListener('input', this.init.bind(this));
-    }
+  constructor(textarea, remainCharacter, btn) {
+    this.text = textarea;
+    this.character = textarea.maxLength;
+    this.remain = remainCharacter;
+    this.btn = btn;
 
-    init() {
-        const remainC = this.character - this.text.value.length;
-        this.remain.textContent = `${remainC} character left!`;
-    }
+    this.text.addEventListener("input", this.init.bind(this));
+  }
+
+  init() {
+    const remainC = this.character - this.text.value.length;
+    this.remain.textContent = `${remainC} character left!`;
+  }
 }
 
-const textarea = document.getElementById('message');
-const remainCharacter = document.querySelector('.character-number');
-const btn = document.querySelector('.btn-send');
+const textarea = document.getElementById("message");
+const remainCharacter = document.querySelector(".character-number");
+const btn = document.querySelector(".btn-send");
 
 const form = new Form(textarea, remainCharacter, btn);
 
-
 class SendForm {
-    constructor(formContainer) {
-        this.formCont = formContainer;
+  constructor(formContainer) {
+    this.formCont = formContainer;
 
-        this.formCont.addEventListener('submit', this.init.bind(this));
+    this.formCont.addEventListener("submit", this.init.bind(this));
+  }
+
+  async init(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    try {
+      const response = await fetch("https://formspree.io/f/mjvqdqql", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (response.ok) {
+        this.formCont.reset();
+        alert("Your message has been sent.");
+      } else {
+        alert("An error occurred. Please try again.");
+      }
+    } catch (error) {
+      console.error(error.message);
     }
-
-    async init(e) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-
-        try {
-            const response = await fetch('https://formspree.io/f/mjvqdqql', {
-                method: 'POST',
-                body: formData,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            })
-
-            if (response.ok) {
-                this.formCont.reset();
-                alert('Your message has been sent.');
-            } else {
-                alert('An error occurred. Please try again.');
-            }
-        } catch (error) {
-            console.error(error.message);
-        }
-    } 
+  }
 }
 
-const formContainer = document.querySelector('.form-container');
-if (formContainer) new SendForm(formContainer); 
+const formContainer = document.querySelector(".form-container");
+if (formContainer) new SendForm(formContainer);
 
+// Footer
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
